bd3dec863678805e396342af2e659e176661672e
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-13 23:42:36 -0300

    feat: 🎸 Checkout page - First commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { BaseLayout } from '@/pages/_layouts/BaseLayout'
+import { Checkout } from '@/pages/Checkout'
 import { Home } from '@/pages/Home'
 import { NotFound } from '@/pages/NotFound/404'
 
@@ -9,7 +10,8 @@ export const router = createBrowserRouter([
         path: '/',
         element: <BaseLayout />,
         children: [
-            { path: '/', element: <Home /> }
+            { path: '/', element: <Home /> },
+            { path: '/checkout', element: <Checkout /> }
         ]
     },
 
